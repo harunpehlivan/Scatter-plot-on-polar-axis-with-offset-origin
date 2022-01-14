8a2257d1a13599ab5b63a5835816e3e8729d7147
HARUN PEHLİVAN <trcmnhp@hotmail.com>
2022-01-14 13:23:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+fig = plt.figure()
+ax = fig.add_subplot(projection='polar')
+c = ax.scatter(theta, r, c=colors, s=area, cmap='hsv', alpha=0.75)
+
+ax.set_rorigin(-2.5)
+ax.set_theta_zero_location('W', offset=10)
